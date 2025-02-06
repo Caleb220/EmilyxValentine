24a2a0961a46a1b19b25700f2a55dae9d722dca5
Caleb W <46548391+Caleb220@users.noreply.github.com>
2025-02-06 14:19:06 +1000

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function moveNoButton() {
     let maxX = window.innerWidth - noButton.offsetWidth - 20; // 20px margin
     let maxY = window.innerHeight - noButton.offsetHeight - 20; // 20px margin
 
-    let x = Math.max(10, Math.random() * maxX);
-    let y = Math.max(10, Math.random() * maxY);
+    let x = Math.max(30, Math.random() * maxX);
+    let y = Math.max(30, Math.random() * maxY);
 
     noButton.style.left = `${x}px`;
     noButton.style.top = `${y}px`;
